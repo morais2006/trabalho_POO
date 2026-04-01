61e9d88b00f4615c646c7cd840585deffbdbb7d1
morais2006 <joamanoel2@gmail.com>
2026-04-01 19:50:52 -0300

    Add Normal class extending Ingresso

---
+++ Normal.java
@@ -0,0 +1,9 @@
+public class Normal extends Ingresso {
+    public Normal(double valor) {
+        super(valor);
+    }
+
+    public void tipoIngresso() {
+        System.out.println("Ingresso Normal");
+    }
+}
