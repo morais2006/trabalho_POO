739e6ac11f4823f588abd129beba44c18d990c46
morais2006 <joamanoel2@gmail.com>
2026-04-01 19:50:25 -0300

    Add Ingresso class with value printing method

---
+++ Ingresso.java
@@ -0,0 +1,11 @@
+public class Ingresso {
+    protected double valor;
+
+    public Ingresso(double valor) {
+        this.valor = valor;
+    }
+
+    public void imprimeValor() {
+        System.out.println("Valor: " + valor);
+    }
+}
