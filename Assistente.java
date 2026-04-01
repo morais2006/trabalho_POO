f24b7b360b296b4d1198bdd1ae15949bb7ad29ed
morais2006 <joamanoel2@gmail.com>
2026-04-01 19:48:22 -0300

    Add Assistente class extending Funcionario

---
+++ Assistente.java
@@ -0,0 +1,22 @@
+public class Assistente extends Funcionario {
+    private int matricula;
+
+    public Assistente(String nome, double salario, int matricula) {
+        super(nome, salario);
+        this.matricula = matricula;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public void exibeDados() {
+        super.exibeDados();
+        System.out.println("Matrícula: " + matricula);
+    }
+}
